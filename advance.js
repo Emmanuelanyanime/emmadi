719afc8d368b82ccc925b718143d625090b1b181
Emmanuelanyanime <chamberskelvin46@gmail.com>
2026-02-05 00:14:59 +0100

    Update advance.js

--- advance.js
+++ advance.js
@@ -1,6 +1,6 @@
 var hourH=document.getElementById("hour");
 var minutesM=document.getElementById("minutes");
-var second=document.getElementById("seconds");
+var secondH=document.getElementById("seconds");
 var AmPmE=document.getElementById("AmPm");
 
 
@@ -14,15 +14,16 @@ function updateClock(){
     h=h-12
     ampm="PM"
    }
- 
+ if (h === 0) h = 12;
+   
    h=h<10 ? "0" + h:h;
     m=m<10 ? "0" + m:m;
      s=s<10 ? "0" + s:s;
 
    hourH.innerText=h;
    minutesM.innerText=m;
-   second.innerText=s;
-   AmPmE,(innerText=ampm;)
+   secondH.innerText=s;
+   AmPmE.innerText=ampm;
    
 
    setTimeout(()=>{
@@ -31,5 +32,6 @@ updateClock()
 }
 
 updateClock()
+
 
 
