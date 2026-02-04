315d9c68938df073b1b45b1fc1716a64a863f2c5
Emmanuelanyanime <chamberskelvin46@gmail.com>
2026-02-04 15:48:02 +0100

    Add files via upload

---
+++ advance.js
@@ -0,0 +1,30 @@
+var hourH=document.getElementById("hour");
+var minutesM=document.getElementById("minutes");
+var second=document.getElementById("seconds");
+var AmPmE=document.getElementById("AmPm");
+
+
+function updateClock(){
+   var h=new Date().getHours();
+   var m=new Date().getMinutes();
+   var s=new Date().getSeconds();
+   var ampm="AM"
+
+   if(h>12){
+    h=h-12
+    ampm="PM"
+   }
+ 
+   h=h<10 ? "0" + h:h;
+    m=m<10 ? "0" + m:m;
+     s=s<10 ? "0" + s:s;
+
+   hourH.innerText=h;
+   minutesM.innerText=m;
+   second.innerText=s;
+
+   setTimeout(()=>{
+updateClock()
+   },1000)
+}
+updateClock()
